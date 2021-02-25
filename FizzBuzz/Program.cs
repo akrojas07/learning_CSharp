a31655c01b2e805576351c7446798d75b68525bf
Anakaren <ak.rojas.07@gmail.com>
2021-02-25 15:04:23 -0800

    First program - learned about Do - While loop, taking user input, ! operator, creating variables and storing values, if else statements, how to use math operators

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter an operand (+ - / *): ");
+            //var keyOperand = Console.ReadLine();
+            //while (keyOperand != ("+") && keyOperand != ("-") && keyOperand != ("*") && keyOperand != ("/"))
+            //{
+            //    Console.WriteLine($"{keyOperand} is not a valid operand. Please enter a valid operand (+ - / *): ");
+            //    keyOperand = Console.ReadLine();
+            //}
+            
+            string keyFirstValue;
+            int firstValue;
+            do
+            {
+                Console.WriteLine("Enter a number: ");
+                keyFirstValue = Console.ReadLine();
+            }
+            while (!int.TryParse(keyFirstValue, out firstValue));
+
+
+            string keySecondValue;
+            int secondValue;
+            do
+            {
+                Console.WriteLine("Enter a non-zero number: ");
+                keySecondValue = Console.ReadLine();
+
+            } 
+            while (!int.TryParse(keySecondValue, out secondValue) || secondValue == 0);
+
+            //Console.WriteLine($"{ secondValue }");
+            //Console.ReadLine();
+
+            int remainder = firstValue % secondValue;
+            int quotient = firstValue / secondValue;
+
+            //Console.WriteLine($"{ remainder }");
+
+            if (remainder == 0)
+            {
+                if (quotient % 5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else 
+                {
+                    Console.WriteLine("Fizz");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Buzz");
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
